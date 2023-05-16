6e88ba01c2e199318ada6e7fe1ce94e78f7ecf03
desmond8643 <128795516+desmond8643@users.noreply.github.com>
2023-05-16 16:15:14 +0800

    Update bookmarklet-master.js

--- Web Scraping/maimai/bookmarklet-master.js
+++ Web Scraping/maimai/bookmarklet-master.js
@@ -1,6 +1,9 @@
-javascript:(function() {   
-    let elements = document.getElementsByClassName("music_master_score_back pointer w_450 m_15 p_3 f_0");   
-    let data = [];   
-    for (let i = 0; i < elements.length; i++) {     
-        data.push(elements[i].innerText);   }   
-    console.log(data); })();
+javascript:(
+    function() {   
+        let elements = document.getElementsByClassName("music_master_score_back pointer w_450 m_15 p_3 f_0");   
+        let data = [];   
+        for (let i = 0; i < elements.length; i++) {     
+            data.push(elements[i].innerText);   
+        }   
+        console.log(data); 
+})();
