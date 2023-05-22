71e02d2223221ce098b457315f36e16f59d0d6cf
desmond8643 <128795516+desmond8643@users.noreply.github.com>
2023-05-22 15:53:03 +0800

    Update rock-paper-scissors.js

--- Mini Projects/Rock paper scissors/scripts/rock-paper-scissors.js
+++ Mini Projects/Rock paper scissors/scripts/rock-paper-scissors.js
@@ -22,7 +22,7 @@ let intervalId;
 
 function autoPlay() {
     if (!isAutoPlaying) {
-        intervalId = setInterval(function() {
+        intervalId = setInterval(() => {
             const playerMove = pickComputerMove();
             playGame(playerMove);
         }, 1000);
@@ -33,7 +33,40 @@ function autoPlay() {
         isAutoPlaying = false;
     }
 }
-    
+
+document.querySelector('.js-rock-button').addEventListener('click', () => {
+    playGame('rock');
+});
+
+document.querySelector('.js-paper-button').addEventListener('click', () => {
+    playGame('paper');
+});
+
+document.querySelector('.js-scissors-button').addEventListener('click', () => {
+    playGame('scissors');
+});
+
+document.querySelector('.js-reset-score-button').addEventListener('click', () => {
+    score.wins = 0;
+    score.losses = 0;
+    score.ties = 0;
+    localStorage.removeItem('score');
+    updateScoreElement();
+});
+
+document.querySelector('.js-auto-play-button').addEventListener('click', () => {
+    autoPlay();
+});
+
+document.body.addEventListener('keydown', (event) => {
+    if (event.key === 'r') {
+        playGame('rock');
+    } else if (event.key === 'p') {
+        playGame('paper');
+    } else if (event.key === 's') {
+        playGame('scissors');
+    }
+});
 
 function playGame(playerMove) {
     const computerMove = pickComputerMove();
